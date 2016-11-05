87559d689f9046e82cec769fb883f08900a3e2b8
g960059 <g960059@gmail.com>
2016-11-05 14:34:24 +0900

    change

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,23 +8,8 @@ const replace = require("gulp-replace");
 const markdown = require('gulp-markdown');
 const html2jade = require('gulp-html2jade');
 const jade = require('gulp-jade');
-var gulpSequence = require('gulp-sequence')
+var gulpSequence = require('gulp-sequence');
 
-
-gulp.task('default',()=>{
-  console.log("test");
-});
-
-gulp.task('copy',()=>{
-  gulp.src(['plugin/menu.js'])
-  .pipe(gulp.dest('dist'));
-});
-
-gulp.task('slides', () => gulp.src('sources/*.pug')
-    .pipe(pug({}))
-    .pipe(reveal())
-    .pipe(gulp.dest('slides/'))
-);
 
 gulp.task('serve', ()=> {
   gulp.src('reveal') //Webサーバーで表示するサイトのルートディレクトリを指定
@@ -39,26 +24,26 @@ var file_name = argv['f'];
 
 
 gulp.task('rename',()=>{
-  gulp.src(file_name)
+  return gulp.src(file_name)
   .pipe(rename('tmp.pug'))
   .pipe(gulp.dest('pug'))
 });
 
 gulp.task('pug', ()=> {
-  gulp.src('pug/index.pug')
+  return gulp.src('pug/index.pug')
   .pipe(pug({pretty: true, basedir:"./"}))
   .pipe(gulp.dest('reveal'))
 });
 
 gulp.task('pug_md', ()=> {
-  gulp.src('pug/index.pug')
+  return gulp.src('pug/index.pug')
   .pipe(pug({pretty: true, basedir:"./"}))
   .pipe(html2jade())
   .pipe(replace("  hr"," section"))
   .pipe(gulp.dest('reveal'))
 });
 gulp.task('jade',()=>{
-  gulp.src('reveal/index.jade')
+  return gulp.src('reveal/index.jade')
   .pipe(jade({
       pretty: true
     }))
@@ -66,7 +51,7 @@ gulp.task('jade',()=>{
 })
 
 gulp.task('watch', ()=>{
-  gulp.watch('pug/*.pug',['rename','pug'])
+  return gulp.watch('pug/*.pug',['rename','pug'])
 })
 
 gulp.task('dev',gulpSequence(['rename','pug','serve','watch']))
